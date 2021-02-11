acdf9bc3f7f2682f2393a6f3bff2d3cc1de4f2b3
Yashbarot1995 <78569674+Yashbarot1995@users.noreply.github.com>
2021-02-11 18:18:01 -0500

    Web page working properly

---
+++ javascript/scripts.js
@@ -0,0 +1,35 @@
+$('.green-button1').on('click', function() {
+	$('#1').css('background-color', 'green')
+})
+
+$('.red-button1').on('click', function() {
+	$('#1').css('background-color', 'red')
+})
+$('.green-button2').on('click', function() {
+	$('#2').css('background-color', 'green')
+})
+
+$('.red-button2').on('click', function() {
+	$('#2').css('background-color', 'red')
+})
+$('.green-button3').on('click', function() {
+	$('#3').css('background-color', 'green')
+})
+
+$('.red-button3').on('click', function() {
+	$('#3').css('background-color', 'red')
+})
+$('.green-button4').on('click', function() {
+	$('#4').css('background-color', 'green')
+})
+
+$('.red-button4').on('click', function() {
+	$('#4').css('background-color', 'red')
+})
+$('.green-button5').on('click', function() {
+	$('#5').css('background-color', 'green')
+})
+
+$('.red-button5').on('click', function() {
+	$('#5').css('background-color', 'red')
+})
